3a42c24d46354161d11c57def65dba33746c6093
So-Eun Yu <so-eunyu@So-Eunui-MacBookPro.local>
2021-05-06 11:01:29 +0900

    revise IntergratedConverter.java
    ---- detail ----
    
    just enter

--- app/src/main/java/edu/handong/csee/java/IntergratedConverter.java
+++ app/src/main/java/edu/handong/csee/java/IntergratedConverter.java
@@ -30,7 +30,6 @@ public class IntegratedConverter {
 
             // You will learn about the try-catch block and Exception in L19
             try {
-
                 // Advanced Java feature: Reflection (it lets us use classes with their String class names.)
                 // (JC does not teach this topic as it is advanced one but you can study by yourself!!)
                 // https://www.oracle.com/technical-resources/articles/java/javareflection.html
@@ -45,7 +44,6 @@ public class IntegratedConverter {
                 System.out.println("The converter (" + converterName + ") for " + originalMeasure + " to " + targetMeasure + " does not exist!!");
             }
         } else {
-
             AllConverter myAllConverter = new AllConverter();
             // When your method returns `this' in methods of AllConverter, you can call them in this way (method chaining).
             // Think/understand how and why this work. Study and search for Method chaining
